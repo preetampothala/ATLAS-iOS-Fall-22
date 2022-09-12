0184ac7242900d2a017ba4901654f55aaa0bf101
Preetam Pothala <preetamreddy.pothala@gmail.com>
2022-09-12 12:26:36 -0600

    Update Contents.swift

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -70,3 +70,38 @@ if let actualNumber = Int(possibleNumber) {
 } else {
     print("The string \"\(possibleNumber)\" couldn't be converted to an integer")
 }
+// Empty array -- Array: [type]=[]
+var someInts: [Int] = []
+print("someInts is of type [Int] with \(someInts.count) items.")
+// appending an array
+someInts.append(3)
+print(someInts)
+someInts=[];
+print(someInts)
+var threeDoubles = Array(repeating: 1.0, count: 3)
+var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
+var sixDoubles = threeDoubles + anotherThreeDoubles
+print(sixDoubles)
+var shoppingList: [String] = ["Eggs", "Milk"]
+var shopppingList:[Any] = ["Eggs", "Milk", 1]
+if shoppingList.isEmpty {
+    print("The shopping list is empty.")
+} else {
+    print("The shopping list isn't empty \(shoppingList)")
+}
+//appending array using += operator
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+print(shoppingList)
+var numbers=[1,2,3,4,5,6]
+numbers[3...5]=[8,9];
+print(numbers)
+numbers.insert(10, at: 4)
+print(numbers)
+let removednumberfromarray=numbers.remove(at: 0)
+numbers.removeLast()
+for number in numbers {
+    print(number)
+}
+for (index,num) in numbers.enumerated(){
+    print("Number \(index + 1): \(num)")
+}
